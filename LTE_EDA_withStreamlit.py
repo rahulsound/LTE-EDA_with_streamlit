ee262312dc0a4b733101589f0d6adbfcf05f4ca0
Rahul Soundrarajan <rahulsound@gmail.com>
2024-08-08 20:23:50 +0530

    requirements

--- LTE_EDA_withStreamlit.py
+++ LTE_EDA_withStreamlit.py
@@ -2,8 +2,8 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import streamlit as st
-from pandas_profiling import ProfileReport
-#from ydata_profiling import ProfileReport
+#from pandas_profiling import ProfileReport
+from ydata_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
 import plotly.express as px
 
